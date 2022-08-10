0cf50df5dd140c0790d6aa42fc17f4fca73e92f7
Cesar Saavedra <cesar.saavedra@domain.com>
2022-08-09 23:18:43 -0500

    csa - testing model validation client

---
+++ WSProductos1001.UnitTest/Shared/ClientMother.cs
@@ -0,0 +1,23 @@
+﻿using WSProductos1001.Entities;
+
+namespace WSProductos1001.UnitTest.Shared;
+
+public static class ClientMother
+{
+    public static EClient Sandy()
+    {
+        return new EClient()
+        {
+            Address = "Calle falsa 123",
+            Identification = "123456789",
+            Names = "Sandy",
+            Phone = "123456789",
+            BirthDay = DateTime.Now,
+            LastNames = "Lopez",
+            MaritalStatus = 1,
+            SpouseIdentification = "123456789",
+            SpouseName = "Juana",
+            SubjectCredit = 2
+        };
+    }
+}
---
+++ WSProductos1001.UnitTest/ValidationsModels/ClientValidationTest.cs
@@ -0,0 +1,30 @@
+﻿using WSProductos1001.Domain.Features.Client;
+using WSProductos1001.UnitTest.Shared;
+
+namespace WSProductos1001.UnitTest.ValidationsModels;
+
+public class ClientValidationTest
+{
+    private ClientValidator _clientValidator;
+    [SetUp]
+    public void Setup()
+    {
+        _clientValidator = new ClientValidator();
+    }
+
+    [Test]
+    public void Client_Validation_ShouldBeOk()
+    {
+        var client = ClientMother.Sandy();
+        var result = _clientValidator.Validate(client);
+        Assert.IsTrue(result.IsValid);
+    }
+    [Test]
+    public void Client_Validation_ShouldBeFail()
+    {
+        var client = ClientMother.Sandy();
+        client.Identification = null;
+        var result = _clientValidator.Validate(client);
+        Assert.IsFalse(result.IsValid);
+    }
+}
